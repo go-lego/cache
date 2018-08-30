1417d4ab6a475aaeb83ebb4b7f212f327fcec043
Henry Sha <shapiming@enjoypiano.cn>
2018-08-30 18:47:03 +0800

    Implement redis driver

--- driver/redis.go
+++ driver/redis.go
@@ -1,7 +1,9 @@
 package driver
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
@@ -88,77 +90,145 @@ func (r *redisDriver) MGet(keys []string) (map[string]string, error) {
 
 // MSet set multiple key-value pairs
 func (r *redisDriver) MSet(kvs map[string]string) error {
+	c := r.pool.Get()
+	defer c.Close()
+	tmp := make([]interface{}, len(kvs)*2)
+	i := 0
+	for k, v := range kvs {
+		tmp[i] = k
+		tmp[i+1] = v
+		i += 2
+	}
+	_, err := redis.String(c.Do("MSET", tmp...))
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // Del delete specified key
 func (r *redisDriver) Del(key string) error {
-	return nil
+	c := r.pool.Get()
+	defer c.Close()
+	_, err := redis.String(c.Do("DEL", key))
+	return err
 }
 
 // Check if the given key exists
 func (r *redisDriver) Exists(key string) (bool, error) {
-	return false, nil
+	c := r.pool.Get()
+	defer c.Close()
+	return redis.Bool(c.Do("EXISTS", key))
 }
 
 // Expire set key expiration
 func (r *redisDriver) Expire(key string, ex int64) error {
-	return nil
+	c := r.pool.Get()
+	defer c.Close()
+	_, err := redis.String(c.Do("EXPIRE", key, ex))
+	return err
 }
 
 // Incr increment key
 func (r *redisDriver) Incr(key string, delta string) (string, error) {
-	return "", nil
+	c := r.pool.Get()
+	defer c.Close()
+	return redis.String(c.Do("INCRBY", key, delta))
 }
 
 // Decr increment key
 func (r *redisDriver) Decr(key string, delta string) (string, error) {
-	return "", nil
+	c := r.pool.Get()
+	defer c.Close()
+	return redis.String(c.Do("DECRBY", key, delta))
 }
 
 // func for hashes
 
 // HGEt get hash key
 func (r *redisDriver) HGet(key string, hk string) (string, error) {
-	return "", nil
+	c := r.pool.Get()
+	defer c.Close()
+	return redis.String(c.Do("HGET", key, hk))
 }
 
 // HSet set hash key
 func (r *redisDriver) HSet(key string, hk string, value string) error {
-	return nil
+	c := r.pool.Get()
+	defer c.Close()
+	_, err := redis.String(c.Do("HSET", key, hk, value))
+	return err
 }
 
 // HMGet get multiple hash keys
 func (r *redisDriver) HMGet(key string, hks []string) (map[string]string, error) {
-	return nil, nil
+	c := r.pool.Get()
+	defer c.Close()
+	tmp := make([]interface{}, len(hks)+1)
+	tmp[0] = key
+	for i, k := range hks {
+		tmp[i+1] = k
+	}
+	return redis.StringMap(c.Do("HMGET", tmp...))
 }
 
 // HMSet set multiple hash keys
 func (r *redisDriver) HMSet(key string, kvs map[string]string) error {
-	return nil
+	c := r.pool.Get()
+	defer c.Close()
+	tmp := make([]interface{}, len(kvs)*2+1)
+	tmp[0] = key
+	i := 1
+	for k, v := range kvs {
+		tmp[i] = k
+		tmp[i+1] = v
+		i += 2
+	}
+	_, err := redis.String(c.Do("HMSET", tmp...))
+	return err
 }
 
 // HGetAll get all hash keys
 func (r *redisDriver) HGetAll(key string) (map[string]string, error) {
-	return nil, nil
+	c := r.pool.Get()
+	defer c.Close()
+	return redis.StringMap(c.Do("HGETALL", key))
 }
 
 // HDel delete hash key
 func (r *redisDriver) HDel(key string, hk string) error {
-	return nil
+	c := r.pool.Get()
+	defer c.Close()
+	_, err := redis.String(c.Do("HDEL", key, hk))
+	return err
 }
 
 // HExists check if the given hash key exists
 func (r *redisDriver) HExists(key string, hk string) (bool, error) {
-	return false, nil
+	c := r.pool.Get()
+	defer c.Close()
+	return redis.Bool(c.Do("HEXISTS", key, hk))
 }
 
 // HIncr increment value of hash key
 func (r *redisDriver) HIncr(key string, hk string, delta string) (string, error) {
-	return "", nil
+	c := r.pool.Get()
+	defer c.Close()
+	return redis.String(c.Do("HINCRBY", key, hk, delta))
 }
 
 // HDecr decrement value of hash key
 func (r *redisDriver) HDecr(key string, hk string, delta string) (string, error) {
-	return "", nil
+	c := r.pool.Get()
+	defer c.Close()
+	nd := ""
+	i := strings.Index(delta, "-")
+	if i == -1 {
+		nd = fmt.Sprintf("-%s", delta)
+	} else if i == 0 {
+		nd = delta[1:]
+	} else {
+		return "", errors.New("driver redis: invalid delta string")
+	}
+	return redis.String(c.Do("HINCRBY", key, hk, nd))
 }
--- driver/redis_test.go
+++ driver/redis_test.go
@@ -59,6 +59,7 @@ func TestRedisMGet(t *testing.T) {
 	}
 
 	c.Command("MGET", "test1", "test2", "testno", "test3").Expect([]interface{}{"1", "2", nil, "test3"})
+
 	ret, err := r.MGet([]string{"test1", "test2", "testno", "test3"})
 	if err != nil {
 		t.Error("No error was expected to MGet, but: ", err)
@@ -68,5 +69,272 @@ func TestRedisMGet(t *testing.T) {
 	}
 	if ret["test1"] != "1" || ret["test2"] != "2" || ret["test3"] != "test3" || ret["testno"] != "" {
 		t.Error("MGet result was incorrect: ", ret)
+	}
+}
+
+func TestRedisMSet(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("MSET", "test1", "ok", "test2", "good").Expect("OK")
+
+	err := r.MSet(map[string]string{"test1": "ok", "test2": "good"})
+	if err != nil {
+		t.Error("No error was expected to MSet, but: ", err)
+	}
+}
+
+func TestRedisDel(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("DEL", "test1").Expect("OK")
+
+	err := r.Del("test1")
+	if err != nil {
+		t.Error("No error was expected to del, but: ", err)
+	}
+}
+
+func TestRedisExists(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("EXISTS", "test1").Expect(int64(1))
+	c.Command("EXISTS", "test2").Expect(int64(0))
+
+	b, err := r.Exists("test1")
+	if err != nil {
+		t.Error("No error was expected to exists, but: ", err)
+	}
+	if !b {
+		t.Error("Key 'test1' should exists")
+	}
+	b, err = r.Exists("test2")
+	if err != nil {
+		t.Error("No error was expected to exist, but: ", err)
+	}
+	if b {
+		t.Error("Key 'test2' should not exist")
+	}
+}
+
+func TestRedisExpire(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("EXPIRE", "test1", int64(123456)).Expect("OK")
+
+	err := r.Expire("test1", int64(123456))
+	if err != nil {
+		t.Error("No error was expected to expire, but: ", err)
+	}
+}
+
+func TestRedisIncr(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("INCRBY", "test1", "13").Expect("100")
+
+	nv, err := r.Incr("test1", "13")
+	if err != nil {
+		t.Error("No error was expected to incr, but: ", err)
+	}
+	if nv != "100" {
+		t.Error("Incr return value incorrect")
+	}
+}
+
+func TestRedisDecr(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("DECRBY", "test1", "13").Expect("100")
+
+	nv, err := r.Decr("test1", "13")
+	if err != nil {
+		t.Error("No error was expected to decr, but: ", err)
+	}
+	if nv != "100" {
+		t.Error("Decr return value incorrect")
+	}
+}
+
+func TestRedisHGet(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("HGET", "test1", "k1").Expect("100")
+
+	v, err := r.HGet("test1", "k1")
+	if err != nil {
+		t.Error("No error was expected to HGet, but: ", err)
+	}
+	if v != "100" {
+		t.Error("HGet return value incorrect")
+	}
+}
+
+func TestRedisHSet(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("HSET", "test1", "k1", "100").Expect("OK")
+
+	err := r.HSet("test1", "k1", "100")
+	if err != nil {
+		t.Error("No error was expected to HSet, but: ", err)
+	}
+}
+
+func TestRedisHMGet(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("HMGET", "test1", "k1", "k2", "k3").Expect([]interface{}{[]byte("k1"), []byte("ok"), []byte("k2"), []byte("good"), []byte("k3"), []byte("1")})
+
+	m, err := r.HMGet("test1", []string{"k1", "k2", "k3"})
+	if err != nil {
+		t.Error("No error was expected to HMGet, but: ", err)
+	}
+	if m["k1"] != "ok" || m["k2"] != "good" || m["k3"] != "1" {
+		t.Error("HMGet return value incorrect")
+	}
+}
+
+func TestRedisHMSet(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("HMSET", "test1", "k1", "ok", "k2", "good", "k3", "1").Expect("ok")
+
+	err := r.HMSet("test1", map[string]string{"k1": "ok", "k2": "good", "k3": "1"})
+	if err != nil {
+		t.Error("No error was expected to HMSet, but: ", err)
+	}
+}
+
+func TestRedisHGetAll(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("HGETALL", "test1").Expect([]interface{}{[]byte("k1"), []byte("ok"), []byte("k2"), []byte("good"), []byte("k3"), []byte("1")})
+
+	m, err := r.HGetAll("test1")
+	if err != nil {
+		t.Error("No error was expected to HMGet, but: ", err)
+	}
+	if m["k1"] != "ok" || m["k2"] != "good" || m["k3"] != "1" {
+		t.Error("HMGet return value incorrect")
+	}
+}
+
+func TestRedisHDel(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("HDEL", "test1", "k1").Expect("OK")
+
+	err := r.HDel("test1", "k1")
+	if err != nil {
+		t.Error("No error was expected to HDel, but: ", err)
+	}
+}
+
+func TestRedisHExists(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("HEXISTS", "test1", "k1").Expect(int64(1))
+	c.Command("HEXISTS", "test1", "k2").Expect(int64(0))
+
+	b, err := r.HExists("test1", "k1")
+	if err != nil {
+		t.Error("No error was expected to HExists, but: ", err)
+	}
+	if !b {
+		t.Error("Key 'test1'.'k1' should exists")
+	}
+	b, err = r.HExists("test1", "k2")
+	if err != nil {
+		t.Error("No error was expected to HExists, but: ", err)
+	}
+	if b {
+		t.Error("Key 'test1'.'k2' should not exist")
+	}
+}
+
+func TestRedisHIncr(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("HINCRBY", "test1", "k1", "13").Expect("100")
+
+	nv, err := r.HIncr("test1", "k1", "13")
+	if err != nil {
+		t.Error("No error was expected to HIncr, but: ", err)
+	}
+	if nv != "100" {
+		t.Error("HIncr return value incorrect")
+	}
+}
+
+func TestRedisHDecr(t *testing.T) {
+	c := redigomock.NewConn()
+	r := &redisDriver{
+		pool: &testRedisPool{conn: c},
+	}
+
+	c.Command("HINCRBY", "test1", "k1", "-13").Expect("100")
+	c.Command("HINCRBY", "test1", "k1", "13").Expect("100")
+
+	nv, err := r.HDecr("test1", "k1", "13")
+	if err != nil {
+		t.Error("No error was expected to HDecr, but: ", err)
+	}
+	if nv != "100" {
+		t.Error("HDecr return value incorrect")
+	}
+
+	nv, err = r.HDecr("test1", "k1", "-13")
+	if err != nil {
+		t.Error("No error was expected to HDecr, but: ", err)
+	}
+	if nv != "100" {
+		t.Error("HDecr return value incorrect")
+	}
+
+	_, err = r.HDecr("test1", "k1", "1-3")
+	if err == nil {
+		t.Error("'Invalid delta' error was expected to HDecr, but: ", err)
 	}
 }
